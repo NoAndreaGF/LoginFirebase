e1b0240aa353fc78931fd9765d5f2ad448c5f846
andrea <andrea.garcia216626@potros.itson.edu.mx>
2022-05-05 11:33:36 -0700

    Initial commit

---
+++ app/src/main/java/garcia/andrea/loginfirebase/User.kt
@@ -0,0 +1,5 @@
+package garcia.andrea.loginfirebase
+
+data class User(var firstName : String ?= null,var lastName : String ?= null,var age : String ?= null){
+    override fun toString() = firstName + "\t" + lastName + "\t" + age
+}
